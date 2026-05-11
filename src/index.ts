d07164353a01122c29399d322c8a1e64a21b258b
adebayodeolu <adebayodeolu@gmail.com>
2026-05-11 11:41:34 +0000

    chore: add barrel export file for all components, hooks, utils, and types

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+// Components
+export { NetworkBadge } from "./components/NetworkBadge/NetworkBadge";
+export { TokenDisplay } from "./components/TokenDisplay/TokenDisplay";
+export { TxStatus } from "./components/TxStatus/TxStatus";
+export { WalletConnect } from "./components/WalletConnect/WalletConnect";
+export { ContractCall } from "./components/ContractCall/ContractCall";
+export { AddressDisplay } from "./components/AddressDisplay/AddressDisplay";
+
+// Hooks
+export { useStellarWallet } from "./hooks/useStellarWallet";
+export { useTxStatus } from "./hooks/useTxStatus";
+export { useContractCall } from "./hooks/useContractCall";
+export { useTokenBalance } from "./hooks/useTokenBalance";
+export { useContractEvents } from "./hooks/useContractEvents";
+
+// Context
+export { NetworkProvider, useNetwork } from "./context/NetworkContext";
+
+// Utils
+export { truncateAddress, isValidStellarAddress, isValidContractId } from "./utils/address";
+export { parseSorobanError, isTxRejected, isInsufficientFunds, isNetworkError } from "./utils/errors";
+export { scValToString, stringToScVal, numberToScVal, scValToNumber } from "./utils/xdr";
+
+// Types
+export type { ContractEvent, BalanceRecord, TxRecord, NetworkType } from "./types";
